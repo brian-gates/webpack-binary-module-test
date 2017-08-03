ce76701769a65aa827dd5549d723daf534281689
Brian Gates <brian.g.gates@gmail.com>
2017-08-03 09:45:35 -0700

    Works

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 var nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     target: 'node', // in order to ignore built-in modules like path, fs, etc.
     externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+    plugins: [
+      new CopyWebpackPlugin([{
+        from: './node_modules/binary-module',
+        to: './node_modules/binary-module',
+      }])
+    ]
 };
